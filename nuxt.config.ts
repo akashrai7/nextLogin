2d689d416fb8190ea6279af51b61642df3fd9f75
Akash <rai.akash667@gmail.com>
2025-07-04 20:56:33 +0530

    change nuxt config ts file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,72 @@
-import { defineNuxtConfig } from "nuxt/config";
+// import { defineNuxtConfig } from "nuxt/config";
 
+// export default defineNuxtConfig({
+//    ssr: true,
+//   app: {
+//     // baseURL: "/trezo-bs/",
+//     head: {
+//       link: [
+//         {
+//           rel: "stylesheet",
+//           href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+//         },
+//         {
+//           rel: "stylesheet",
+//           href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0",
+//         },
+//       ],
+//     },
+//   },
+
+//   css: [
+//     "bootstrap/dist/css/bootstrap.min.css",
+//     "swiper/css",
+//     "swiper/css/pagination",
+//     "swiper/css/navigation",
+//     "quill/dist/quill.snow.css",
+//     "/assets/css/sidebar-menu.css",
+//     "/assets/css/simplebar.css",
+//     "/assets/css/remixicon.css",
+//     "/assets/scss/style.css",
+//   ],
+
+//   plugins: [
+//     "~/plugins/apexcharts.client.ts",
+//     "~/plugins/vue-quill.js",
+//     "~/plugins/bootstrap.client.ts",
+//   ],
+//   modules: ["@bootstrap-vue-next/nuxt", "nuxt-swiper", "nuxt-feather-icons"],
+
+//   vue: {
+//     compilerOptions: {
+//       isCustomElement: (tag) => tag.toLowerCase() === "script",
+//     },
+//   },
+
+//   nitro: {
+//     output: {
+//       dir: "dist",
+//       serverDir: "dist/server",
+//     },
+//   },
+
+// runtimeConfig: {
+//     MONGODB_URI: process.env.MONGODB_URI,
+//   },
+
+//  pages: true,
+//   components: true,
+//   compatibilityDate: "2025-01-31",
+// });
 export default defineNuxtConfig({
-   ssr: true,
+  ssr: true,
+
   app: {
-    // baseURL: "/trezo-bs/",
+    // baseURL: "/trezo-bs/", // इसे बंद ही रहने दो
     head: {
       link: [
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0",
-        },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" },
       ],
     },
   },
@@ -35,6 +88,7 @@ export default defineNuxtConfig({
     "~/plugins/vue-quill.js",
     "~/plugins/bootstrap.client.ts",
   ],
+
   modules: ["@bootstrap-vue-next/nuxt", "nuxt-swiper", "nuxt-feather-icons"],
 
   vue: {
@@ -44,13 +98,10 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    output: {
-      dir: "dist",
-      serverDir: "dist/server",
-    },
+    preset: "vercel",
   },
 
-runtimeConfig: {
+  runtimeConfig: {
     MONGODB_URI: process.env.MONGODB_URI,
   },
 
