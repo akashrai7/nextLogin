b0ba32b99167a5026edc52d654b727e9e08d53a1
Akash <rai.akash667@gmail.com>
2025-07-05 20:52:24 +0530

    login page and api creat

--- components/Modules/Authentication/Login/index.vue
+++ components/Modules/Authentication/Login/index.vue
@@ -57,13 +57,13 @@
             <div class="form-group">
               <p>
                 Don’t have an account..
-                <NuxtLink
+                <!-- <NuxtLink
             to="/authentication/login"
             class="btn btn-outline-primary-div py-2 px-4 fw-medium fs-16 rounded-3"
           >
             <i class="ri-login-box-line fs-18 position-relative top-2"></i>
             <span class="ms-1">Login</span>
-          </NuxtLink>
+          </NuxtLink> -->
           <NuxtLink
             to="/authentication/register"
             class="btn btn-primary-div py-2 px-4 fw-medium fs-16 text-white ms-3 rounded-3"
