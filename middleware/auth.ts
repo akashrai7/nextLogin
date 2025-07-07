42f6a2ce8e3c81fb40acc87a789c03e8cccbeeb9
Akash <rai.akash667@gmail.com>
2025-07-07 13:39:16 +0530

    change nuxt.config file

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,21 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const token = process.client ? localStorage.getItem('token') : null;
+  const token = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
 
   if (!token) {
+    return navigateTo('/authentication/login');
+  }
+
+  // Role based check
+  if (to.path.startsWith('/super-admin') && role !== 'superadmin') {
+    return navigateTo('/authentication/login');
+  }
+
+  if (to.path.startsWith('/admin') && role !== 'admin') {
+    return navigateTo('/authentication/login');
+  }
+
+  if (to.path.startsWith('/user') && role !== 'user') {
     return navigateTo('/authentication/login');
   }
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,10 +64,10 @@ export default defineNuxtConfig({
   app: {
     // baseURL: "/trezo-bs/", // इसे बंद ही रहने दो
     head: {
-      // link: [
-      //   { rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" },
-      //   { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" },
-      // ],
+      link: [
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" },
+      ],
     },
   },
 
